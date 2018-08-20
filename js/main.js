e60e49a033057990135a2e0f02158b267b6246b3
southl <gyrii84@gmail.com>
2018-08-20 12:22:19 +0300

    started jquery plugins

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.btn1').click(function(){
+        $('#exampleModal').arcticmodal();
+    });
+    $('.btn2').click(function(){
+        $('#exampleModal2').arcticmodal();
+    });
+});
